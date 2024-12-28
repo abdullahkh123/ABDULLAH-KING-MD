a9b1334123d81936ec7b31994efe561eaa750244
PROxANSAR <panhwarking003@gmail.com>
2024-12-28 20:20:05 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -45,15 +45,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -115,15 +115,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -190,15 +190,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -246,15 +246,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -302,15 +302,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `⁣https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -353,15 +353,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -412,15 +412,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -461,15 +461,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -514,15 +514,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
@@ -564,15 +564,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/MNpy6DM/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: 'Panhwar🫀💀👑',
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: '⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°',
                         serverMessageId: 143
                     }
                 }
