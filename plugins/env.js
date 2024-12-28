985f0d34a57944c3a145364f170e523a7db69e2b
PROxANSAR <panhwarking003@gmail.com>
2024-12-28 08:34:59 +0500

    Update env.js

--- plugins/env.js
+++ plugins/env.js
@@ -47,15 +47,15 @@ async (conn, mek, m, { from, quoted, reply }) => {
         await conn.sendMessage(
             from,
             {
-                image: { url: '' }, // Image URL
+                image: { url: 'https://i.ibb.co/Wnf2wJk/Manul-Ofc-X.jpg' }, // Image URL
                 caption: envSettings,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363321103874131@newsletter',
-                        newsletterName: "Panhwar🫀💀👑",
+                        newsletterJid: '120363358664877093@newsletter',
+                        newsletterName: "⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•°",
                         serverMessageId: 143
                     }
                 }
