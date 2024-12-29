a62e72d7ab91817482aab9e1d6fc806ba4445c8f
⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•° <jadoongg6@gmail.com>
2024-12-29 11:17:12 +0500

    Update down.js

--- plugins/down.js
+++ plugins/down.js
@@ -10,7 +10,7 @@ let baseUrl;
 })();
 
 
-const yourName = "*PANHWAR MD🧬*";
+const yourName = "*ABDULLAH-KING MD🧬*";
 
 //twitter dl (x)
 cmd({
@@ -26,7 +26,7 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
         if (!q && !q.startsWith("https://")) return reply("give me twitter url")
         //fetch data from api  
         let data = await fetchJson(`${baseUrl}/api/twitterdl?url=${q}`)
-        reply("*PANHWAR MD TWITTER VIDEO DOWNLOADING...📥*")
+        reply("*ABDULLAH KING MD TWITTER VIDEO DOWNLOADING...📥*")
         //send video (hd,sd)
         await conn.sendMessage(from, { video: { url: data.data.data.HD }, mimetype: "video/mp4", caption: `- HD\n\n ${yourName}` }, { quoted: mek })
         await conn.sendMessage(from, { video: { url: data.data.data.SD }, mimetype: "video/mp4", caption: `- SD \n\n ${yourName}` }, { quoted: mek })  
