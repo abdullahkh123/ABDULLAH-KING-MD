f782a5473393234ba7ce9f4658ee44bfa2ccea0a
⁣♛꧁ 𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇༒𓆩 𝐊𝐈𝐍𝐆 .𓆪༒𝐌𝐃꧂⁣♛•° <jadoongg6@gmail.com>
2024-12-31 12:56:06 +0500

    Update down.js

--- plugins/down.js
+++ plugins/down.js
@@ -52,7 +52,7 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
         if (!q && !q.startsWith("https://")) return reply("give me gdrive url")
         //fetch data from api  
         let data = await fetchJson(`${baseUrl}/api/gdrivedl?url=${q}`)
-        reply("*ABDULLAH-KING MD GOOGLE DRIVE FILE DOWNLOADING...📥*")
+        reply("*ABDULLAH khan MD GOOGLE DRIVE FILE DOWNLOADING...📥*")
         await conn.sendMessage(from, { document: { url: data.data.download }, fileName: data.data.fileName, mimetype: data.data.mimeType, caption: `${data.data.fileName}\n\n${yourName}` }, { quoted: mek })                                                                                                                 
     } catch (e) {
         console.log(e)
